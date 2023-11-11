1aa14eeda163e41e8e31af64f34263dbf62741f3
daninavagar <daninavagar@gmail.com>
2023-11-11 17:07:03 +0100

    keys supabase

--- src/plugins/subapase.ts
+++ src/plugins/subapase.ts
@@ -4,12 +4,16 @@ import { createClient } from "@supabase/supabase-js";
 // const url = process.env.SUPABASE_URL
 // const anonkey = process.env.SUPABASE_ANON_KEY
 
-const SUPABASE_URL = 'https://cswthhgqgqtashimoimz.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzd3RoaGdxZ3F0YXNoaW1vaW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTczODA3MDUsImV4cCI6MjAxMjk1NjcwNX0.b7fZEHONC4da-PqigmDkVZ4fO_lew9HStlkz833y3SE'
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || 'default_url';
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'default_key';
+
+console.log(SUPABASE_URL);
+console.log(SUPABASE_ANON_KEY);
+
 const supabase = createClient(
     SUPABASE_URL,
-    SUPABASE_KEY
-)
+    SUPABASE_ANON_KEY
+);
 
 export default supabase;
 
