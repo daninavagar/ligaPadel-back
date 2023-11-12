65702e3db03ced42005c8a06279306b4d6b83da4
daninavagar <daninavagar@gmail.com>
2023-11-12 16:14:47 +0100

    supabase changes

--- src/plugins/subapase.ts
+++ src/plugins/subapase.ts
@@ -1,18 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
-// const url = process.env.SUPABASE_URL
-// const anonkey = process.env.SUPABASE_ANON_KEY
+const url = process.env.SUPABASE_URL || '';
+const anonkey = process.env.SUPABASE_ANON_KEY || '';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || 'default_url';
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'default_key';
+console.log('url ', url);
+console.log('anonkey ', anonkey);
 
-console.log(SUPABASE_URL);
-console.log(SUPABASE_ANON_KEY);
 
 const supabase = createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    url,
+    anonkey
 );
 
 export default supabase;
