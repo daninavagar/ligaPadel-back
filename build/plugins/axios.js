f721bbe56d4b560ad6ae6ea3e4d019bcf36abf4e
daninavagar <daninavagar@gmail.com>
2023-11-28 20:51:13 +0100

    quito timeout

--- build/plugins/axios.js
+++ build/plugins/axios.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const http = axios_1.default.create({
-    timeout: 5000,
     headers: {
         'X-Api-Key': 's_FqokQ2xLeFyauGdFJ#TmwBhN_Li#FRa%IcA%rDosp@4nmWQ_23v-3xzK1%w0Bw'
     }
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const http = axios.create({
-    timeout: 5000,
     headers: {
         'X-Api-Key': 's_FqokQ2xLeFyauGdFJ#TmwBhN_Li#FRa%IcA%rDosp@4nmWQ_23v-3xzK1%w0Bw'
     }
